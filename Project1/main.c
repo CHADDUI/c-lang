654b529814f75d70c5cde6bfaf096b2b0c2be7d3
CHADDUI <shckdrud1@gmail.com>
2025-05-09 00:43:46 +0900

    공부한 코드드

---
+++ Project1/main.c
@@ -0,0 +1,699 @@
+//#include <stdio.h>
+//
+//int main(void) {
+//
+//	int x = 10,
+//	int y = 010,
+//	int z = 0x10,
+//
+//	printf("x = % d" x);
+//	printf("y = % d", y);
+//	printf("z = % d", z);
+//
+//	return 0;
+
+//(24)
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	double x, y, result;
+//
+//	printf("두개의 실수를 입력하시오:");
+//	scanf("%lf %lf", &x, &y);
+//
+//	result = x + y;
+//	printf("%f + %f = %f", x, y, result);
+//
+//	result = x - y;
+//	printf("%f - %f = %f", x, y, result);
+//
+//	return 0;
+//}
+
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	double x, y, result;
+//
+//	printf("두 개의 실수를 입력하시오: ");
+//	int check = scanf("%lf %lf", &x, &y);
+//
+//	result = x + y;
+//	printf("%.2f + %.2f = %.2f\n", x, y, result);
+//
+//	result = x - y;
+//	printf("%.2f - %.2f = %.2f\n", x, y, result);
+//
+//	return 0;
+//} 챗
+
+
+//#include <stdio.h>
+//#define SEC_PER_MINUTE 60
+//
+//int main(void)
+//[
+//	int input, minute, second;
+//
+//	print(" 초를 입력하시오:");
+//	scanf
+//]
+
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int x = 10, y = 10;
+//
+//	printf("x=%d\n", x);
+//	printf("++x의 값 = %d\n", ++x);
+//	printf("x=%d\n", x);
+//
+//	printf("y=%d\n", y);
+//	printf("y++의 값=%d\n", y++);
+//	printf("y=%d\n", y);
+//
+//	return 0;
+//}챗
+
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int x=10,y=10,z=33;
+//
+//	x+=1;
+//	y*=2;
+//	z%=10 + 20;
+//
+//	printf("x = %d Y = %d z = %d\n", x, y, z);
+//	return 0;
+//}
+
+//
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int x, y;
+//	
+//	printf("두개의 정수를 입력하시오: ");
+//	int check = scanf("&d &d", &x, &y);
+//	
+//	printf("x == y의 결과값:%d", x == y);
+//	printf("x != y의 결과값:%d", x != y);
+//	printf("x > y의 결과값:%d", x > y);
+//	printf("x < y의 결과값:%d", x < y);
+//	printf("x <= y의 결과값:%d", x <= y);
+//	printf("x >= y의 결과값:%d", x >= y);
+//	
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//    int x, y;
+//
+//    printf("두 개의 정수를 입력하시오: ");
+//
+//    int check = scanf("%d %d", &x, &y);
+//
+//    printf("x == y의 결과값: %d\n", x == y);
+//    printf("x != y의 결과값: %d\n", x != y);
+//    printf("x > y의 결과값: %d\n", x > y);
+//    printf("x < y의 결과값: %d\n", x < y);
+//    printf("x <= y의 결과값: %d\n", x <= y);
+//    printf("x >= y의 결과값: %d\n", x >= y);
+//
+//    return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	printf("AND : %08X\n", 0x9 & 0xA);
+//	printf("OR : %08X\n", 0x9 | 0xA);
+//	printf("XOR : %08X\n", 0x9 ^ 0xA);
+//	printf("NOT : %08X\n", 0x9 - 0xA);
+//	printf("<<:%08\n", 0x4 << 1);
+//	printf(">>:%08\n", 0x4 >> 1);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int a = 32;
+//	a = -a;
+//	a = a + 0x01;
+//	printf("a= %d\n")
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	char data = 'a';
+//	char key = 0xff;
+//	char encrpeted_date, oring
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	char c;
+//	int i;
+//	float f;
+//}
+
+//#define _ CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int i;
+//	double f;
+//	f = 5 / 4;
+//	printf("%f\n", f);
+//	printf
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int number;
+//
+//	printf("정수를 입력하시오:")
+//		scanf("%d", &number);
+//	if (number > 0)
+//printf("didtndlqslek")
+//
+//printf('+"dlqfurehlsrkqdns%ddkqskej')
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int number;
+//
+//	printf("정수를 입력하시오:");
+//	int check = scanf("%d", &number);
+//	if (number = 0)
+//		number = -number;
+//	printf("절대값은 %d 입니다:");
+//	return 0;
+//
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int number;
+//
+//	printf("정수를 입력하시오:");
+//	int check = scanf("%d", &number):
+//	if (number%==2)
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main(void)
+//{
+//	int n, d, result;
+//	printf("분자와 분모를 입력하시오");
+//	scanf("%d%d", &n, &d);
+//	if (d == 0)
+//	{
+//		printf("0으로 나눌수 없습니다.\n");
+//	}
+//	else
+//	{
+//		result = n / d;
+//		printf("n / d = %d ", result);
+//	}
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int score;
+//	printf("성적을 입력하시오:");
+//	scanf("%d", &score);
+//
+//	
+//	
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	char ch	
+//	printf("문자를 입력하시오:");
+//	scanf("%d",)
+//}
+
+//#include <stdio.h>
+// case=
+
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int i = 1;
+//loop:
+//	printf("%d * %d = %d\n", 3, i, 3 * i);
+//	i++;
+//	if (i == 10) goto end;
+//	goto loop;
+//end;
+//	return 0;
+//}
+
+//#define _ CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main(void)
+//{
+//	int incam exenpt;
+//	double rate; tax;
+//	scanf("%d", kacae);
+//	printf(누진공제액을);
+//	scanf("%d", kacae);
+//	if (10억 <= income)
+//	{
+//		rake = 0.45;
+//		ex
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int i = 0;
+//	while(i < 5);
+//	{
+//		printf("hello world!\n");
+//		i++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int n;
+//	int i = 1;
+//	printf("출력하고 싶은 단:");
+//	scanf("%d", &n);
+//
+//	while (i == 9)
+//	{
+//		printf("%d"%d=%d\n,"n")
+//
+//
+//#include <stdio.h>
+//int main(void)
+//{
+//	int n;
+//
+//
+//
+//}#include <stdio.h>
+//int main(void)
+//#include <stdio.h>}#include <stdio.h>
+//}
+//
+//#include <stdio.h>
+//int main(void)
+//
+//int n;
+//
+//wh
+
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int i = 2;
+//	
+//	while (i<10)
+//	{
+//		printf("%d은 참입니다", i);
+//		i++;
+//	}
+//	printf("%d 은 씹새입니다", i);
+//
+//}
+
+//#define _ crt_secure_no_warnings
+//#include <stdio.h>
+//int main(void)
+//{
+//	int grade, n;
+//	float sum, average;
+//	n = 0;
+//	sum = 0;
+//	grade = 0;
+//
+//	printf("성적 입력을 종료하시려면 홀수를 입력하시오\n");
+//
+//	while (grade >= 0)
+//	{
+//		printf("성적을 입력하시오:");
+//		scanf("%d", &grade);
+//
+//		sum += grade;
+//		n++;
+//
+//	}
+//
+//	sum = sum - grade;
+//	n--;
+//	average = sum / n;
+//}
+
+//#define _ crt_secure_no_warnings
+//#include <stdio.h>
+//int main(void)
+//{
+//	int halflife;
+//	double initial;
+//	double current;
+//	int years = 0;
+//
+//	printf("반감기를 입력하시오(년):");
+//	scanf("%d", &halflife);
+//
+//	initial = 100.0;
+//	current = initial;
+//	while (current > initial / 10.0) {
+//		years += halflife;
+//		current = current / 2.0;
+//		printf("%d년 후에 남은 양=%f", years, current);
+//	}
+//	printf("1/10 이하로 되기까지 걸린 시간 = %d", years);
+//	return 0;
+//
+//}
+//
+//#define _ crt_secure_no_warnings
+//#include <stdio.h>
+//int main(void)
+//{
+//	int number, sum = 0
+//.}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int i = 0;
+//	do
+//	{
+//		printf("1 새로만들기\n");
+//		printf()
+//	}
+//}
+//#include <stdio.h>
+//int main(void)
+//{
+//	int i;
+//	//i = 2;
+//	for (i = 6 ; i < 5; i++)
+//	printf("hello world\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	long fact = 1;
+//	int i = 1, n;
+//	printf("정수를 입력하시오:");
+//	scanf("%d", &n);
+//	while (i <= n)
+//	{
+//		fact = fact * i;
+//		i++;
+//	}
+//	printf("&di는 %d입니다.", n, fact);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int i;
+//	for (i = 0;i < 10;i++)
+//	{
+//		if (i % 3 == 0)
+//			continue;
+//		printf("%d", i);
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	char letter;
+//	while (1)
+//	{
+//		printf("소문자를 입력하시오:");
+//		scanf("%c", &letter);
+//
+//		if (letter == 'Q')
+//			break;
+//		if (letter < 'a' || letter > 'z')
+//			continue;
+//		letter = 32;
+//		printf("변환된 대문자는 %c입니다.\n", letter);
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main(void)
+//{
+//	int intal_money = 50;
+//	int goal = 250;
+//	int i;
+//	int wins = 0;
+//	srand(unsigned)time(NULL);
+//	for (i = 0; i < 100; i++) {
+//		int cash = initial_money;
+//		
+//	}
+//}
+
+//#include <stdio.h>
+//void happyBirthday()
+//{
+//	printf("생일축하 합니다!\n");
+//	printf("생일축하 합니다!\n");
+//	printf("사랑하는 친구의");
+//	printf("생일축하 합니다!\n");
+//}
+//int main(void)
+//{
+//	happyBirthday();
+//	return 0;
+//}
+
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//void printmenu()
+//{
+//	printf("===============================\n");
+//	printf("'c'섭씨온도에서 화씨온도로 변환\n");
+//	printf("'f'화씨온도에서 섭씨온도로 변환\n");
+//	printf("'q'는 종료\n");
+//	printf("===============================\n");
+//	
+//}
+//double C2F(double c_temp)
+//{
+//	return 9.0 / 5.0 * c_temp + 32.0;
+//}
+//double F2C(double f_temp)
+//{
+//	return (f_temp - 32.0) * 5.0 / 9.0;
+//}
+//int main(void)
+//{
+//	char choice;
+//	double temp;
+//	while (1) {
+//		printmenu();
+//		printf("메뉴에서 선택하세요:");
+//		choice = getchar();
+//		if (choice == 'q') break;
+//		else if (choice == 'c') {
+//			printf("섭씨온도:");
+//			scanf("%lf", &temp);
+//			printf("화씨온도:%lf\n\n", C2F(temp));
+//		}
+//		else if (choice == 'f') {
+//			printf("화씨온도:");
+//			scanf("%lf", &temp);
+//			printf("섭씨온도: %lf \n\n", F2C(temp));
+//		}
+//		getchar();
+//	}
+//	return 0;
+//}
+
+
+//함수원형
+
+//#include <stdilb.h>
+//#include <stdio.h>
+//#include <time.h>
+//
+//int coin_toss(void);
+//int main(void)
+//{
+//	int toss;
+//	int heads = 0;
+//	int tails = 0;
+//	srand((unsigned)time(NULL));
+//
+//	for(toss = 0; toss <100 )
+//
+//}
+// 
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <conio.h>
+//#include <time.h>
+
+
+//#include <stdio.h>
+//#include <math.h>
+//int main(void)
+//{
+//	double pi = 3.1415926535;
+//	double x, y;
+//}
+
+//#include <stdlib.h>
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	system("dir");
+//	printf("아무 키나 치세요\n");
+//	_getch();
+//	system("cls");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int A;
+//int B;
+//int add()
+//{
+//	return A + B;
+//}
+//int main(void)
+//{
+//	int answer;
+//	A = 5;
+//	B = 7;
+//	answer = add();
+//	printf()
+//}
+
+//#include<stdio.h>
+//
+//int sum = 1;
+//
+//int main(void)
+//{
+//	int sum = 0;
+//	
+//	printf("sum = %d\n", sum);
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+// 
+//void save(int amount)
+//{
+//	static long balance = 0;
+//
+//	if (amount >= 0)
+//		printf("%d\t\t", amount);
+//	else
+//		printf("\t%d\t", -amount);
+//
+//	balance += amount;
+//	printf("%d\n", balance);
+//}
+//
+//int main(void) {
+//	printf("=====================\n");
+//	printf("입금 \t출금\t 잔고\n");
+//}
+//#include<stdio.h>
+//#include<stdarg.h>
+//int sum(jnt, ...);
+//int main(void)
+//{
+//	int answer = sum(4, 4, 3, 2, 1);
+//	printf("합은 %d입니다.\n", answer);
+//	return 0;
+//}
+//int sum(int num, ...)
+//{
+//	int answer = 0;
+//
+//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int day;
+		scanf("%d", &day);
+	switch (day)
+	{
+	case 1:
+		printf("1");
+		break;
+	case 2:
+		else if ( day == 2) {
+			printf("%d", day)
+		}
+		printf("2");
+		break;
+	case 3:
+		printf("3");
+		break;
+	default:
+		printf("18");
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
